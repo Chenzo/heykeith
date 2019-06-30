aaa6aedd86e180c2f699a41db3d14dcbcdbd99bf
Vince Suriani <chenzorama@hotmail.com>
2019-06-30 12:24:09 -0400

    almost have data working

--- src/components/JsonGetter.js
+++ src/components/JsonGetter.js
@@ -2,23 +2,45 @@
 import React, {Component} from "react";
 import {hot} from "react-hot-loader";
 
+import "./JsonGetter.css";
+
 class Jgetter extends React.Component {
   constructor(props) {
     super(props);
-    //this.state.fdata = {};
+    this.state = {
+        uData: [],
+        gotData: false
+    };
   }
 
 
-  gogetit() {
 
+  showData() {
+    /* const element = (
+        <div>
+            <h4>DATA GOT</h4>
+        </div>
+      );
+      ReactDOM.render(element, document.getElementById('jsonout')); */
+  }
+
+  componentDidMount() {
+        
     var request = new XMLHttpRequest();
-    request.open('GET', '/data/fruit.json', true);
+    var me = this;
+    request.open('GET', '/data/users.json', true);
 
     request.onload = function() {
         if (request.status >= 200 && request.status < 400) {
             // Success!
             var data = JSON.parse(request.responseText);
             console.log(data);
+            me.setState({
+                uData: data,
+                gotData: true
+            });
+
+            me.showData();
         } else {
             // We reached our target server, but it returned an error
 
@@ -31,23 +53,37 @@ class Jgetter extends React.Component {
 
     request.send();
 
-    return "ding";
-  }
 
-  componentDidMount() {
-        const ff = this.gogetit();
         console.log("SHOULD HAVE FIRED");
     }
 
   render() {
-    return (
-    <div>
-        Data GOT
-    </div>
-    );
+    if(!this.state.done) {
+        return (
+        <div className="jsonGetter">Got it
+            <ul className="jsonGetter">
+                
+            </ul>
+        </div>
+        );
+    } else {
+        return (
+        <div className="jsonGetter">Loading...</div>
+        );
+    }
+    
+    
   }
 }
 
 
 export default hot(module)(Jgetter);
 
+/*
+
+            /* {
+            this.state.uData.map(
+                user => {
+                return <li>{ user.name }</li>;
+                })
+            } */
